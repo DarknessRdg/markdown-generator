ab3afa1077ffe1917c2f39f1f634cc2d813cfd7e
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-02-19 23:32:22 -0300

    docs: add some examples

---
+++ example/class.py
@@ -0,0 +1,35 @@
+"""
+# Class files
+
+This file is an example as how your code should be done with classes.
+"""
+
+
+class MyClassExample:
+    """
+    This class shows how to start your markdowns.
+    My class with a simple example of how to documentation your classes
+
+    **Args:**
+        `count`: int
+    """
+
+    def __init__(self, count=0):
+        """
+        Class constructor
+
+        **Args**:
+            `count`: int
+        """
+
+        self.count = count
+
+    def get_count(self):
+        """
+        This method is used to get count's current value
+
+        **Returns:**
+            int: current value of count
+        """
+
+        return self.count
---
+++ example/function.py
@@ -0,0 +1,18 @@
+"""
+# Functions files
+
+This file is an example as how your code should be done with functions.
+"""
+
+
+def get_started():
+    """
+    This functions shows how to start your markdowns
+
+    It is very simple, just place your docstring as a markdown text and you're
+    ready yo go.
+
+    **Returns:**
+        A great experience!!
+    """
+    return

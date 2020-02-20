08f4171660705a0b3da9a8ea62e551c784f848ff
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-02-19 23:51:40 -0300

    feat: add generate_python_md

--- generate.py
+++ generate.py
@@ -1,1 +1,32 @@
+import os
 
+SRC_FOLDER = ''
+
+
+def is_python_file(file_name):
+    return file_name.endswith('.py') and file_name != '__init__.py'
+
+
+def is_not_hidden_folder(file_name):
+    return len(file_name.split('.')) == 1 and file_name != 'venv'
+
+
+def generate_python_md(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            print(line.strip())
+
+
+def generate():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    files_path = '%s/%s/' % (base_dir, SRC_FOLDER)
+    for file_name in sorted(os.listdir(files_path)):
+
+        if is_python_file(file_name):
+            path = files_path + '/%s' % file_name
+            generate_python_md(path)
+
+
+if __name__ == '__main__':
+    generate()

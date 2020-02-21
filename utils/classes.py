133360222e7cbe3eb335fb1a01f924032b271c0c
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-02-20 23:42:50 -0300

    feat: add classes utils

--- utils/classes.py
+++ utils/classes.py
@@ -1,3 +1,24 @@
-class Function:
-    def __init__(self, dosctring):
-        self.doc = dosctring
+class BaseObject:
+    """
+    Class to store function data
+    Attributes:
+        doc: function's docstring
+    """
+    def __init__(self):
+        self.doc = ''
+
+
+class Tree:
+    """
+    Tree to store an objects children
+    Attributes:
+        current: current objects
+        children: list of current's children objects
+    """
+
+    def __init__(self, current):
+        self.current = current
+        self.children = []
+
+    def __len__(self):
+        return len(self.current)

3e57aae377362ccc4724b0dd056ebd3a3b0fe627
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-11-05 11:42:50 -0300

    test: add tests for function that gets indent

---
+++ tests/indented_language/test_get_indent.py
@@ -0,0 +1,84 @@
+import logging
+
+import pytest
+
+from indented_language import get_indent
+
+
+FILE_TEXT = '''
+def function():
+    pass
+
+
+def function_nested():
+
+    def function_nested_child():
+        pass
+    pass
+
+
+def function_with_docs_space():
+
+    pass
+
+
+class MyClass:
+
+    class Meta:
+        model = MyModel
+
+    def method_1(self):
+        pass
+
+    def get_method_nested(self):
+        def get_method_nested_child():
+            pass
+
+'''
+
+# Functions index in file
+FUNCTION = 0
+FUNCTION_NESTED = 4
+FUNCTION_NESTED_CHILD = 6
+FUNCTION_WITH_DOCS_SPACE = 11
+
+CLASS = 16
+CLASS_META = 18
+CLASS_METHOD_1 = 21
+CLASS_METHOD_NESTED = 24
+CLASS_METHOD_NESTED_CHILD = 25
+
+
+@pytest.fixture()
+def file():
+    split = FILE_TEXT.split('\n')
+    split.pop(0)
+
+    return split
+
+
+def test_happy_function(file):
+    assert get_indent(file, FUNCTION) == 4
+
+
+def test_nested_function(file):
+    assert get_indent(file, FUNCTION_NESTED) == 4
+    assert get_indent(file, FUNCTION_NESTED_CHILD) == 8
+
+
+def test_when_function_has_empty_spaces_before_starts_the_code(file):
+    assert get_indent(file, FUNCTION_WITH_DOCS_SPACE) == 4
+
+
+def test_class_indent(file):
+    assert get_indent(file, CLASS) == 4
+    assert get_indent(file, CLASS_META) == 8
+
+
+def test_class_method(file):
+    assert get_indent(file, CLASS_METHOD_1) == 8
+
+
+def test_class_method_with_nested_function(file):
+    assert get_indent(file, CLASS_METHOD_NESTED) == 8
+    assert get_indent(file, CLASS_METHOD_NESTED_CHILD) == 12

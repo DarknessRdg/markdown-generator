4b81f7bfe9ed0054232dd463eed6f63b765b06c4
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-11-04 21:49:37 -0300

    feat: start new script generator for a indented language

---
+++ indented_language.py
@@ -0,0 +1,56 @@
+import os
+import re
+
+# Relative path to BASE_DIR where code to be
+# generated are stored
+SRC_FOLDER = ''
+
+# Relative path to BASE_DIR where all output `.md` files
+# should be created
+SAVE_FOLDER = ''
+
+# Default number of spaces used to ident code
+INDENT = 4
+
+# Extension name of the file should be generated.
+# ATTENTION: Remember it must be an indented language extension
+FILE_EXTENSION = 'py'
+
+# Base directory to relative all other folders
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Regex of folders names to ignore and not generate file markdowns
+IGNORE_FOLDERS = [
+
+]
+
+# Regex of file names to ignore and not generate file markdowns
+IGNORE_FILES_NAME = [
+    r'^__.*__$',  # python files
+    r'^_.*',  # private files
+]
+
+
+def is_file(file_name, extension=FILE_EXTENSION):
+    parts = file_name.split('.')
+    if len(parts) == 2:
+        name, file_ext = parts
+        return name and file_ext == extension
+    return False
+
+
+def is_allowed_files(file_name, ignore=None):
+    if ignore is None:
+        ignore = IGNORE_FILES_NAME
+
+    if not is_file(file_name):
+        return False
+
+    file_name, _ = file_name.split('.')
+
+    for regex in map(re.compile, ignore):
+        print(regex)
+        if regex.match(file_name):
+            return False
+    return True
+

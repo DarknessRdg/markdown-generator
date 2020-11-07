d1b72b423912ef5d38439f9ba67d2b4887c80c89
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-11-07 09:00:13 -0300

    test: add test for function that gets docstring

---
+++ tests/indented_language/test_get_docstring.py
@@ -0,0 +1,37 @@
+import pytest
+
+from indented_language import get_docstring
+
+
+FILE = '''
+def function():
+    """
+    Returns:
+        - `None`: ...
+    """
+
+def test_docs_with_many_blank_spaces():
+    """
+            This is my cods
+    """
+'''
+
+FUNCTION = 0
+FUNCTION_WITH_MANY_BLANK_SPACES = 6
+
+
+@pytest.fixture(scope='module')
+def file():
+    split = FILE.split('\n')
+    split.pop(0)
+    return split
+
+
+def test_returns_docs_without_white_space(file):
+    docs = get_docstring(file, FUNCTION)
+    assert docs == '''Returns:\n    - `None`: ...\n'''
+
+
+def test_do_not_remove_extra_spaces(file):
+    expected = '        This is my cods\n'
+    assert get_docstring(file, FUNCTION_WITH_MANY_BLANK_SPACES) == expected
